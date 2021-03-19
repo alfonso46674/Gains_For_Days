a790b008ba10923f0f13b4ff151ce1dc89a0ee2c
JPRR44 <juanp.robles@hotmail.com>
2021-03-19 16:38:04 -0600

    Feat: added bloc to exercices

---
+++ lib/ejercicios/bloc/ejercicios_bloc_state.dart
@@ -0,0 +1,24 @@
+part of 'ejercicios_bloc_bloc.dart';
+
+@immutable
+abstract class EjerciciosBlocState {}
+
+class EjerciciosBlocInitial extends EjerciciosBlocState {}
+
+class EjerciciosState extends EjerciciosBlocInitial {
+  @override
+  // TODO: implement props
+  List<Object> get props => throw UnimplementedError();
+}
+
+class EjercicioMusculoState extends EjerciciosBlocInitial {
+  @override
+  // TODO: implement props
+  List<Object> get props => throw UnimplementedError();
+}
+
+class EjercicioDetalleState extends EjerciciosBlocInitial {
+  @override
+  // TODO: implement props
+  List<Object> get props => throw UnimplementedError();
+}
